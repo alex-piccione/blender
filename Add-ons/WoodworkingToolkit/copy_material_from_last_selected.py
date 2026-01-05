ee62f359bc11a061889f0f1f4b0f2b27bc23178e
Alex <alessandro.piccione.75@gmail.com>
2026-01-05 18:19:10 +0100

    correct name

--- Add-ons/WoodworkingToolkit/copy_material_from_last_selected.py
+++ Add-ons/WoodworkingToolkit/copy_material_from_last_selected.py
@@ -2,7 +2,7 @@ import bpy
 
 
 class WOODWORKING_OT_copy_material_from_last_selected(bpy.types.Operator):
-    """Copy the Material from teh last selected object to all the selected ones"""
+    """Copy the Material from the last selected object to all the selected ones"""
     bl_idname = "woodworking.copy_material_from_last_selected"
     bl_label = "Copy Material Object"
     bl_options = {'REGISTER', 'UNDO'}  # {'REGISTER', 'UNDO'}  with REGISTER we have the REDO panel
@@ -20,6 +20,6 @@ class WOODWORKING_OT_copy_material_from_last_selected(bpy.types.Operator):
 
 # In your UI draw function:
 def draw_operator(layout):
-    layout.operator("woodworking.copy_material_to_selected", 
+    layout.operator("woodworking.copy_material_from_last_selected", 
                    text="Copy Material from last selected", 
                    icon='MATERIAL')
